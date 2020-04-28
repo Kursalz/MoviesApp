67039ad98ce3211413b44ceb788ac3071cc690fc
Lorenzon99 <matteolorenz99@gmail.com>
2020-04-28 17:23:16 +0200

    created ApiAdapter

--- app/src/main/java/com/d100/moviesappprova/adapter/ApiAdapter.java
+++ app/src/main/java/com/d100/moviesappprova/adapter/ApiAdapter.java
@@ -2,7 +2,6 @@ package com.d100.moviesappprova.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +13,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.d100.moviesappprova.R;
 import com.d100.moviesappprova.activity.DetailActivity;
-import com.d100.moviesappprova.data.TableHelper;
+import com.d100.moviesappprova.model.Movie;
 
-public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+import java.util.List;
+
+public class ApiAdapter extends RecyclerView.Adapter<ApiAdapter.MyViewHolder> {
     private Context mContext;
-    private Cursor mCursor;
+    private List<Movie> mListMovies;
 
-    public MoviesAdapter(Context aContext, Cursor cursor) {
-        this.mContext = aContext;
-        this.mCursor = cursor;
+    public ApiAdapter(Context aContext, List<Movie> aListMovies) {
+        mContext = aContext;
+        mListMovies = aListMovies;
     }
 
     @NonNull
@@ -34,17 +35,15 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHold
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        mCursor.moveToPosition(position);
-        String poster_path = mCursor.getString(mCursor.getColumnIndexOrThrow(TableHelper.POSTER_PATH));
         Glide.with(mContext)
-                .load(poster_path)
-                .placeholder(R.drawable.load)//load è una gif del loading
-                .into(holder.thumbnail);
+            .load(mListMovies.get(position).getPoster_path())
+            .placeholder(R.drawable.load)//load è una gif del loading
+            .into(holder.thumbnail);
     }
 
     @Override
     public int getItemCount() {
-        return mCursor.getCount();
+        return mListMovies.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -62,15 +61,18 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHold
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    int position = getAdapterPosition();
-                    mCursor.moveToPosition(position);
-                    if (position != RecyclerView.NO_POSITION) {
+                    int vPosition = getAdapterPosition();
+                    Movie vMovie = mListMovies.get(vPosition);
+
+                    if (vPosition != RecyclerView.NO_POSITION) {
                         Intent vIntent = new Intent(mContext, DetailActivity.class);
-                        vIntent.putExtra(ORIGINAL_TITLE, mCursor.getString(mCursor.getColumnIndex(TableHelper.ORIGINAL_TITLE)));
-                        vIntent.putExtra(BACKDROP_PATH, mCursor.getString(mCursor.getColumnIndex(TableHelper.BACKDROP_PATH)));
-                        vIntent.putExtra(OVERVIEW, mCursor.getString(mCursor.getColumnIndex(TableHelper.OVERVIEW)));
-                        vIntent.putExtra(VOTE_AVERAGE, mCursor.getDouble(mCursor.getColumnIndex(TableHelper.VOTE_AVERAGE)));
-                        vIntent.putExtra(RELEASE_DATE, mCursor.getString(mCursor.getColumnIndex(TableHelper.RELEASE_DATE)));
+
+                        vIntent.putExtra(ORIGINAL_TITLE, vMovie.getOriginal_title());
+                        vIntent.putExtra(BACKDROP_PATH, vMovie.getBackdrop_path());
+                        vIntent.putExtra(OVERVIEW, vMovie.getOverview());
+                        vIntent.putExtra(VOTE_AVERAGE, vMovie.getVote_average());
+                        vIntent.putExtra(RELEASE_DATE, vMovie.getRelease_date());
+
                         vIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         mContext.startActivity(vIntent);
                     }

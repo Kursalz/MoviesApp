08d802ff5302892a8703cee1fa0c98f21ecef24a
Matteo Lonigro <matteorallyx@hotmail.it>
2020-05-08 15:51:01 +0200

    Add Icon and SplashScreen

---
+++ app/src/main/java/com/d100/moviesappprova/activity/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.d100.moviesappprova.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.d100.moviesappprova.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_layout);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
